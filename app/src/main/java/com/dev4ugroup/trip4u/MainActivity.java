39525734b82e188c541977cc2677aa5dba63d6f3
dev4ugroup <dev4ugroup@gmail.com>
2017-05-17 18:33:03 +0700

    update theme2

--- app/src/main/java/com/dev4ugroup/trip4u/MainActivity.java
+++ app/src/main/java/com/dev4ugroup/trip4u/MainActivity.java
@@ -1,9 +1,7 @@
 package com.dev4ugroup.trip4u;
 
+
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -23,14 +21,7 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -77,20 +68,20 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
+
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.maps) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.flash) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nearGasStation) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nearMotel) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.settings) {
+
+        } else if (id == R.id.infomation) {
 
         }
 
---
+++ app/src/main/java/com/dev4ugroup/trip4u/slidelist/Settings.java
@@ -0,0 +1,8 @@
+package com.dev4ugroup.trip4u.slidelist;
+
+/**
+ * Created by darkp on 5/17/2017.
+ */
+
+public class Settings {
+}
