52fff72c19e1f35e9a2541396f440e78fe141ad4
dev4ugroup <dev4ugroup@gmail.com>
2017-05-19 21:27:35 +0700

    update button

--- app/src/main/java/com.dev4ugroup.trip4u/MainActivity.java
+++ app/src/main/java/com.dev4ugroup.trip4u/MainActivity.java
@@ -4,7 +4,9 @@ package com.dev4ugroup.trip4u;
 
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,8 +14,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-
-
+import android.view.View;
 
 
 public class MainActivity extends AppCompatActivity
@@ -27,6 +28,15 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Code setting here", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
 
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
